f949c9d6552c6dbcffc31665cef2fdc55cdbbcbf
sajjad <sajjadzolghadr777@gmail.com>
2026-07-24 14:57:20 +0330

    feat: create Consume model for food tracking

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Food(models.Model):
@@ -9,3 +10,9 @@ class Food(models.Model):
     calories = models.IntegerField()
     def __str__(self):
         return self.name
+
+class Consume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    food_consume = models.ForeignKey(Food, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.username + " consumed " + self.food_consume.name
